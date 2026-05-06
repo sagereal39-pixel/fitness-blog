36de970f3625cd66566a74eeb9a46e8febaeb346
sagereal39-pixel <sagereal39@gmail.com>
2026-05-06 22:14:29 +0100

    Final css linking fix

--- api/path.php
+++ api/path.php
@@ -1,4 +1,5 @@
 <?php
+// blog/api/path.php
 define("ROOT_PATH", realpath(dirname(__FILE__) . '/../'));
 
 $host = $_SERVER['HTTP_HOST'];
@@ -6,6 +7,6 @@ $host = $_SERVER['HTTP_HOST'];
 if ($host == 'localhost:5001') {
   define("BASE_URL", "http://localhost:5001/blog");
 } else {
-  // Replace this with your ACTUAL Vercel URL temporarily
-  define("BASE_URL", "https://fitness-blog-kdfbsrsfm-sagereal39-3189s-projects.vercel.app");
+  // Use https explicitly to stop the browser from blocking your styles[cite: 8]
+  define("BASE_URL", "https://" . $host);
 }

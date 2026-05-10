6e949236157518a492c1fa9949d4a7136e8f5575
sagereal39-pixel <sagereal39@gmail.com>
2026-05-10 05:48:38 +0100

    Fix: Updated CSS for optimal mobile view and session active warnings

--- api/path.php
+++ api/path.php
@@ -5,7 +5,7 @@ define("ROOT_PATH", realpath(dirname(__FILE__) . '/../'));
 $host = $_SERVER['HTTP_HOST'];
 
 if ($host == 'localhost:5001') {
-  define("BASE_URL", "http://localhost:5001/blog");
+  define('BASE_URL', 'https://fitness-blog-dh1lb398j-sagereal39-3189s-projects.vercel.app');
 } else {
   // Use https explicitly for Vercel
   define("BASE_URL", "https://" . $host);

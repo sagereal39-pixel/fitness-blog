d1258480f1ae79072cae07a241894f014d90143b
sagereal39-pixel <sagereal39@gmail.com>
2026-05-11 06:06:12 +0100

    Fix 3: Levelled post slider cards and enforced uniform image cropping

--- api/index.php
+++ api/index.php
@@ -64,8 +64,6 @@ if (isset($_GET['t_id'])) {
 
     <div class="post-slider">
       <h1 class="slider-title">Trending Posts</h1>
-      <i class="fas fa-chevron-left prev"></i>
-      <i class="fas fa-chevron-right next"></i>
 
       <div class="post-wrapper">
         <?php foreach ($posts as $post): ?>

018ee74413c4e6dfd3d487eb3c81b412f5df67cc
sagereal39-pixel <sagereal39@gmail.com>
2026-05-12 15:18:51 +0100

    Updated media queries an session.php

--- app/includes/session.php
+++ app/includes/session.php
@@ -1,7 +1,7 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) {
-  // Only set these if we don't have an active session cookie yet
-  if (!headers_sent() && !isset($_COOKIE[session_name()])) {
+  // Only set these if a session hasn't started yet
+  if (!headers_sent()) {
     ini_set('session.cookie_path', '/');
     ini_set('session.cookie_samesite', 'Lax');
     ini_set('session.cookie_httponly', '1');
